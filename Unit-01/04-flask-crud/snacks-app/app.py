d0625fd23f24e9bdb2a983287b878ef2e92d5a03
tostaylo <tostaylo1@gmail.com>
2017-02-08 19:23:48 -0800

    worked up to delete on CRUD

--- Unit-01/04-flask-crud/snacks-app/app.py
+++ Unit-01/04-flask-crud/snacks-app/app.py
@@ -1,28 +1,57 @@
-from flask import Flask, render_template, url_for, request, redirect
-from snack import Snack
+from flask import Flask, render_template, request, redirect, url_for
 from flask_modus import Modus
+from snack import Snack
 
-pb = Snack('Peanut Butter','http://peanutbutterlovers.com/wp-content/uploads/2015/10/pb_hero.jpg')
-print(pb)
 
-snacks = [pb]
+
+
 
 app = Flask(__name__)
 modus = Modus(app)
 
-@app.route('/snacks', methods=["GET", "POST"])
-def index():
-  if request.method == "POST":
-    snacks.append(Snack(request.form['name'], request.form['image_url']))
-    return redirect(url_for('index'))
+@app.route('/')
+def hello():
+    return "HELLO WORRRRRRRRLD"
 
-  return render_template('index.html', snacks=snacks)
+@app.route('/snacks', methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        Snack(request.form['name'], request.form['image_url'])
+        return redirect(url_for('index'))
+    return render_template('index.html', snacks=Snack.snack_list)
+
 
 @app.route('/snacks/new')
 def new():
-  return render_template('new.html')
+    return render_template('new.html', snacks=Snack.snack_list)
+
+@app.route('/snacks/<int:id>',methods=["GET","PATCH"])
+def show(id):
+    if request.method == b"PATCH":
+        Snack.find(id).name = request.form['name']
+        return redirect(url_for('index'))
+    return render_template('show.html', snack=Snack.find(id))
+
+@app.route('/snacks/<int:id>/edit')
+def edit(id):
+
+    return render_template('edit.html', snack=Snack.find(id))
+
+
+#Snack('snack','sndsaf')
+#print(Snack.find(1).name)
+
+
+
+
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
-  app.run(port=3000, debug=True)
+   app.run(port=3000, debug = True)
+   #—never use debug true in production. Use only in development
--- Unit-01/04-flask-crud/snacks-app/snack.py
+++ Unit-01/04-flask-crud/snacks-app/snack.py
@@ -1,11 +1,20 @@
 class Snack():
-    count = 1
 
-    def __init__(self, name, image_url):
+    count = 1
+    snack_list = []
+
+    def __init__ (self, name, image_url):
         self.name = name
         self.image_url = image_url
         self.id = Snack.count
-        Snack.count += 1
+        Snack.count +=1
+        Snack.snack_list.append(self)
 
-    def __repr__(self):
-        return "Snack #{}: {}".format(self.id, self.name)
+
+    @classmethod
+    def find(cls, snack_id):
+        return next(snack for snack in cls.snack_list if snack.id == snack_id)
+
+
+
+
