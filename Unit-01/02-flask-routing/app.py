5c7e8a8cd08e933fc76211b47683fe5c96ed336d
Matt Barraza-Carl <mccarl@asu.edu>
2017-08-16 00:40:40 -0700

    Complete parts 1 & 2

--- Unit-01/02-flask-routing/app.py
+++ Unit-01/02-flask-routing/app.py
@@ -2,29 +2,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/math')
-def math_functions():
-    return "Math is Fun 🎠 !"
-
-@app.route('/add/<int:num>/<int:num>')
+@app.route('/add/<int:num_1>/<int:num_2>')
 def add(num_1, num_2):
-    sum = num_1 + num_2
-    return str(sum)
-
-@app.route('/subtract/<int:num>/<int:num>')
+    return str(num_1 + num_2)
+#
+@app.route('/subtract/<int:num_1>/<int:num_2>')
 def subtract(num_1, num_2):
-    subtraction = num_1 - num_2
-    return str(subtraction)
+    return str(num_1 - num_2)
 
-@app.route('/multiply/<int:num>/<int:num>')
-def name(num_1, num_2):
-    multiplication = num_1 * num_2
-    return str()
+@app.route('/multiply/<int:num_1>/<int:num_2>')
+def multiply(num_1, num_2):
+    return str(num_1 * num_2)
 
-@app.route('/divide/<int:num>/<int:num>')
-def name(num_1, num_2):
-    division = num_1 / num_2
-    return str()
+@app.route('/divide/<int:num_1>/<int:num_2>')
+def divide(num_1, num_2):
+    try:
+        quotient = num_1 /num_2
+    except ZeroDivisionError:
+        return "Please do not divide by 0"
+    return str(quotient)
+#
+@app.route('/math/<calculator>/<int:num_1>/<int:num_2>')
+def math_functions(calculator, num_1,num_2):
+    if calculator == "add":
+        return str(num_1 + num_2)
+    elif calculator == "subtract":
+        return str(num_1 - num_2)
+    elif calculator == "multiply":
+        return str(num_1 * num_2)
+    try:
+        quotient = num_1 / num_2
+    except ZeroDivisionError:
+        return "🚯"
+    return str(quotient)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
