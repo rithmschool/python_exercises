0f573f637c4f57b834cc6da9939adadf7b4ae645
Kevin Chang <kevincchang13@gmail.com>
2017-08-17 14:05:42 -0700

    forgot to push finished exercises

--- Unit-01/04-flask-crud/snack.py
+++ Unit-01/04-flask-crud/snack.py
@@ -1,1 +1,10 @@
 # Add a class for a snack here!
+class Snack:
+
+	snack_id = 1
+
+	def __init__(self, name, kind):
+		self.name = name
+		self.kind = kind
+		self.id = Snack.snack_id
+		Snack.snack_id +=1
