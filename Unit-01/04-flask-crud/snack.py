e8b326182ff161854ceb774a3e8fb16465929403
Michelle Huynh <michhh.12@gmail.com>
2017-08-17 17:28:58 -0700

    finished CRUD snack app. currently working on putting in the database

--- Unit-01/04-flask-crud/snack.py
+++ Unit-01/04-flask-crud/snack.py
@@ -1,1 +1,8 @@
-# Add a class for a snack here!
+class Snack():
+	count = 1
+	def __init__(self, name, type, deliciousness):
+		self.name = name
+		self.type = type
+		self.deliciousness = deliciousness
+		self.id = Snack.count
+		Snack.count += 1
