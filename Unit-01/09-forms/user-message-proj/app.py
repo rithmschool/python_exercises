45434f9f53942c34c01cd9ee097d819106473d33
Adele Landers <adelethefish@gmail.com>
2017-08-22 20:45:23 -0700

    Working until Edit/Delete

--- Unit-01/09-forms/user-message-proj/app.py
+++ Unit-01/09-forms/user-message-proj/app.py
@@ -1,2 +1,11 @@
 ##flask-sql-alchemy is my virtenv
+from flask import Flask,render_template,url_for,request,redirect
+from flask_modus import Modus 
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+modus = Modus(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/users'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
