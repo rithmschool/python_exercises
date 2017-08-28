6415579bfc75a07eba1748d52ac6d29c4b7bb6c5
Rob Conner <rjconner@mac.com>
2017-08-28 16:30:25 -0700

    users resource on blueprint homework done, starting messages

---
+++ Unit-02/01-blueprints/project/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_modus import Modus
+import os
+
+app = Flask(__name__)
+modus = Modus(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask-sql-blueprints'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+db = SQLAlchemy(app)
+
+# import a blueprint that we will create - Notice the placement of the import!
+# it is essential that you do not import that blueprint before you initialize your app 
+# and your db variable - this will cause errors with circular imports!
+from project.users.views import users_blueprint
+from project.messages.views import messages_blueprint
+
+# register our blueprints with the application
+app.register_blueprint(users_blueprint, url_prefix='/users')
+app.register_blueprint(messages_blueprint, url_prefix='/messages')
+
+@app.route('/')
+def root():
+    return "HELLO BLUEPRINTS!"
