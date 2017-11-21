ebc60dfded567ca22b01a9172a388e07498ef8e5
michellecarey <genovese28@yahoo.com>
2017-11-21 07:26:45 -0800

    updates

--- Unit-01/01-flask-intro/app.py
+++ Unit-01/01-flask-intro/app.py
@@ -1,4 +1,4 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,14 +7,13 @@ def welome():
 	print(" Welcome!")
 	return "Hello, welcome to my app!"
 
-@app.route("/Welcome home")
+@app.route("/welcome/home")
 def welcome_home():
 	return "Welcome home!"
 
-@app.route("Welcome back!")
-def Welcome_back():
+@app.route("/welcome/back!")
+def welcome_back():
 	return "Welcome back!"
 
-	if __name__ == "__main__":
-		app.run(port=8080, 
-			debug=True)
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
