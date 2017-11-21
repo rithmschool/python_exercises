2916f53df8dffdd64b08a6d6270c619457785285
alliejant <alliejant@gmail.com>
2017-11-20 21:11:08 -0800

    fixed spacing

--- Unit-01/01-flask-intro/app.py
+++ Unit-01/01-flask-intro/app.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "You are in the right place"
+  return "You are in the right place"
 
 @app.route("/welcome")
 def welcome():
-	return "welcome"
+  return "welcome"
 
 @app.route("/welcome/home")
 def welcome_home():
-	return "welcome home"
+  return "welcome home"
 
 @app.route("/welcome/back")
 def welcome_back():
-	return "welcome back"
+  return "welcome back"
 
 @app.route("/sum")
 def sum():
-	return str(5+5)
+  return str(5+5)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+  app.run(debug=True)
