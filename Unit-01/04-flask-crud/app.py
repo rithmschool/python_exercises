56e06411fb5ecd45d2fdee6b872968b1bca40cbd
alliejant <alliejant@gmail.com>
2017-11-26 19:18:39 -0800

    adding basic template

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,3 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_modus import Modus
 
-snack_list = []
+app = Flask(__name__)
+modus = Modus(app)
+
+@app.route("/")
+def root():
+	return redirect(url_for('index'))
+
+#To read items (GET /items)
+#To create item (POST /items)
+@app.route("/items", methods=["GET", "POST"])
+def index():
+	if(request.method == "POST")
+		pass
+		return redirect("url_for('index')")
+	return render_template("index.html")
+
+#To read form to create item(GET /items/new)
+@app.route("/items/new")
+def new():
+	return render_template("new.html")
+
+#To read item (GET /items/<int:id>)
+#To edit item (PUT /items/<int:id>)
+#To delete item (DELETE /items/<int:id>)
+@app.route("/items/<int:id>", methods=["GET", "PUT", "DELETE"])
+def show(id):
+	if(request.method == "PUT")
+		pass
+		return redirect("url_for('show')")
+	if(request.method == b"DELETE")
+		pass
+		return redirect("url_for('show')")
+	return render_template("show.html")
+
+#To read form to edit/delete item (GET /items/<int:id>/edit)
+@app.route("/items/<int:id>/edit")
+def edit_item():
+	return render_template("edit.html")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
