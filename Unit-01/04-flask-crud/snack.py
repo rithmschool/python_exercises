657c675da5bb7ac903cfd6738e6a0e98847822ab
annabrit <annabrit@gmail.com>
2017-11-26 22:29:41 -0800

    adding files for snack CRUD app

--- Unit-01/04-flask-crud/snack.py
+++ Unit-01/04-flask-crud/snack.py
@@ -1,1 +1,23 @@
 # Add a class for a snack here!
+class Snack():
+	id = 1
+	def __init__(self, name, kind):
+		self._name = name
+		self._kind = kind
+		self.id = Snack.id
+		Snack.id += 1
+	def __repr__(self):
+		return "Name: {self.name}; Kind: {self.kind}"	
+	@property
+	def name(self):
+		return self._name
+	@name.setter
+	def name(self, value):
+		self._name = value
+
+	@property
+	def kind(self):
+		return self._kind
+	@kind.setter
+	def kind(self, value):
+		self._kind = value				
