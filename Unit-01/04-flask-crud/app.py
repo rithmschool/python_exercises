c8fd9a28c67c30eadfa4ce6ae68c0b75499f734c
alliejant <alliejant@gmail.com>
2017-11-26 22:37:53 -0800

    finished flask-crud but still need css styling
    removed comment

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,44 +1,59 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
+from snack import Snack
+from functions import find_snack
 from flask_modus import Modus
 
 app = Flask(__name__)
 modus = Modus(app)
 
+snacks = [Snack('kit-kat', 'unhealthy')]
+
 @app.route("/")
 def root():
 	return redirect(url_for('index'))
 
-#To read items (GET /items)
-#To create item (POST /items)
-@app.route("/items", methods=["GET", "POST"])
+#To read snacks (GET /snacks)
+#To create snack (POST /snacks)
+@app.route("/snacks", methods=["GET", "POST"])
 def index():
-	if(request.method == "POST")
-		pass
-		return redirect("url_for('index')")
-	return render_template("index.html")
+	if(request.method == "POST"):
+		snacks.append(Snack(request.form.get("name"), request.form.get("kind")))
+		return redirect(url_for('index'))
+	return render_template("index.html", snacks=snacks)
 
-#To read form to create item(GET /items/new)
-@app.route("/items/new")
+#To read form to create snack(GET /snacks/new)
+@app.route("/snacks/new")
 def new():
 	return render_template("new.html")
 
-#To read item (GET /items/<int:id>)
-#To edit item (PUT /items/<int:id>)
-#To delete item (DELETE /items/<int:id>)
-@app.route("/items/<int:id>", methods=["GET", "PUT", "DELETE"])
+#To read snack (GET /snacks/<int:id>)
+#To edit snack (PUT /snacks/<int:id>)
+#To delete snack (DELETE /snacks/<int:id>)
+@app.route("/snacks/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def show(id):
-	if(request.method == "PUT")
-		pass
-		return redirect("url_for('show')")
-	if(request.method == b"DELETE")
-		pass
-		return redirect("url_for('show')")
-	return render_template("show.html")
+	found = find_snack(id, snacks)
+	if found == None:
+		abort(404)
+	if(request.method == b"PATCH"):
+		found.name = request.form.get("name")
+		found.kind = request.form.get("kind")
+		return redirect(url_for('show', id=found.id))
+	if(request.method == b"DELETE"):
+		snacks.remove(found)
+		return redirect(url_for('index'))
+	return render_template("show.html", snack = found)
 
-#To read form to edit/delete item (GET /items/<int:id>/edit)
-@app.route("/items/<int:id>/edit")
-def edit_item():
-	return render_template("edit.html")
+#To read form to edit/delete snack (GET /snacks/<int:id>/edit)
+@app.route("/snacks/<int:id>/edit")
+def edit(id):
+	found = find_snack(id, snacks)
+	if found == None:
+		abort(404)
+	return render_template("edit.html", snack = found)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
