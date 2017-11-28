1b920f9bdf04d8619f444211d690a086bd08d806
Sean C. Mangosing <seanmangosing@gmail.com>
2017-11-28 13:22:04 -0800

    Finished Exercise

--- Unit-01/01-flask-intro/welcome-app/app.py
+++ Unit-01/01-flask-intro/welcome-app/app.py
@@ -1,3 +1,23 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/welcome")
+def index():
+    return "<h1>Welcome</h1>"
+
+@app.route("/welcome/home")
+def home():
+    return "<h1>Welcome Home</h1>"
+
+@app.route("/welcome/back")
+def back():
+    return "<h1>Welcome Back</h1>"
+
+@app.route("/sum")
+def sum():
+    sum = 5 + 5 
+    return format(sum)
+
+if __name__ == "__main__":
+    app.run(debug=True)
