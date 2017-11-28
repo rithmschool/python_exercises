4151f760eb0ba25a1eb43a16cfce3f1e62ba0495
michellecarey <genovese28@yahoo.com>
2017-11-27 17:04:04 -0800

    completed snacks

--- snacks/app.py
+++ snacks/app.py
@@ -10,7 +10,10 @@ modus = Modus(app)
 snacks = []
 
 def find_snack(id):
-	return [snack for snack in snacks if snack.id == id][0]
+	goodies = [snack for snack in snacks if snack.id == id]
+	if not goodies:
+		return None
+	return goodies[0]
 
 @app.route('/')
 def root():
@@ -19,29 +22,35 @@ def root():
 @app.route('/snacks', methods=["GET", "POST"])
 def index():
 	if request.method == 'POST':
-		new_snack = Snack(request.form['snack'])
+		new_snack = Snack(request.form['name'], request.form['kind'])
 		snacks.append(new_snack)
 		return redirect(url_for('index'))	
 	return render_template('index.html', snacks=snacks)
+
 
 @app.route('/snacks/new')
 def new():
 	return render_template('new.html')
 
-@app.route('/snacks/<int: id>', methods=["GET", "PATCH"])
+@app.route('/snacks/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def show(id):
 	found_snack = find_snack(id)
+	if found_snack is None:
+		return render_template("404.html"), 404
 	if request.method == b'PATCH':
-		found_snack.name = request.form['snack']
+		found_snack.name = request.form['name']
+		found_snack.kind = request.form['kind']
 		return redirect(url_for('index'))
 	if request.method == b'DELETE':
 		snacks.remove(found_snack)
 		return redirect(url_for('index'))
 	return render_template('show.html', snack=found_snack)
 
-@app.route('/snacks/<int: id>/edit')
+@app.route('/snacks/<int:id>/edit')
 def edit(id):
 	found_snack = find_snack(id)
+	if found_snack is None:
+		return render_template("404.html"), 404
 	return render_template('edit.html', snack=found_snack)
 
 
