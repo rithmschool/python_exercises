6af41354f486dbd7273de93ee42af668bd8a42d2
Sean C. Mangosing <seanmangosing@gmail.com>
2017-11-28 21:28:36 -0800

    updating files

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,3 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from snack import Snack
 
-snack_list = []
+app = Flask(__name__)
+
+doritos = Snack(name = "doritos", kind = "chips")
+snickers = Snack(name = "snickers", kind = "candy bar")
+peanuts = Snack(name = "peanuts", kind = "nuts")
+
+snack_list = [doritos, snickers, peanuts]
+
+@app.route("/snacks")
+def index():
+    return render_template('index.html', snacks=snack_list)
+
+
+
+if __name__ == ("__main__"):
+    app.run(debug=True, port=3000)
+
