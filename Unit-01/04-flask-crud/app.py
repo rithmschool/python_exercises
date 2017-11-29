5d248310bef798329e48d1b02cad8cd76ac5184d
rebel_skum <jette.hernan@gmail.com>
2017-11-28 17:19:02 -0800

    setting up template files and app.py

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from snack import Snack
+
 
 app = Flask(__name__)
 
+snickers = Snack(name = "snickers", kind = "candy bar")
+cheetos = Snack(name = 'cheetos', kind = 'chips')
+skittles = Snack(name = 'skittles', kind = 'candy')
+peanuts = Snack(name = 'peanuts', kind = 'nuts')
+
+snack_list = [snickers, cheetos, skittles, peanuts]
+
+@app.route('/snacks')
+def index():
+    return render_template('index.html', snacks=snack_list)
+
+@app.route('/snacks/new')
+def new():
+    return render_template('new.html')
 
 if __name__ == ("__main__"):
-    app.run(debug=True)
-
-snack_list = []
+    app.run(debug=True, port=5000)
