184dd0ca052a15ceacc5598bae95e98ba59e4a91
nakkurt <andre.nakkurt@gmail.com>
2017-11-29 18:42:01 -0800

    init commit

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,3 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from snack import Snack
 
-snack_list = []
+app = Flask(__name__)
+
+pringles = Snack(name='pringles', kind='chips')
+cake = Snack(name='cake', kind='baked goods')
+cupcakes = Snack(name='cupcakes', kind='baked goods')
+rice krispies = Snack(name='rice krispies', kind='baked goods')
+cheetos = Snack(name='cheetos', kind='chips')
+
+snack_list = [pringles, cake, cupcakes, rice krispies, cheetos]
+
+@app.route('/snacks')
+def index():
+    return render_template('index.html', snacks=snacks)
+
+if __name__ == '__main__':
+    app.run(debug=True,port=3000)      
--- Unit-01/04-flask-crud/snack.py
+++ Unit-01/04-flask-crud/snack.py
@@ -1,1 +1,8 @@
-# Add a class for a snack here!
+class Snack():
+
+    count = 1
+
+    def __init__(self,name):
+        self.name = name
+        self.id = Snack.count
+        Snack.count += 1
