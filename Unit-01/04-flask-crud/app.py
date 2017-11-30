cf98e715b70c774a26194e5d96eca156e5e1f50f
nakkurt <andre.nakkurt@gmail.com>
2017-11-29 22:16:31 -0800

    Unit01_04-Flask-Crud_Andre_answers

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -1,19 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from flask_modus import Modus
 from snack import Snack
 
 app = Flask(__name__)
+modus = Modus(app)
+
 
 pringles = Snack(name='pringles', kind='chips')
-cake = Snack(name='cake', kind='baked goods')
-cupcakes = Snack(name='cupcakes', kind='baked goods')
-rice krispies = Snack(name='rice krispies', kind='baked goods')
+cake = Snack(name='cake', kind='baked_goods')
+cupcake = Snack(name='cupcake', kind='baked_goods')
+rice_krispies = Snack(name='rice_krispies', kind='baked_goods')
 cheetos = Snack(name='cheetos', kind='chips')
 
-snack_list = [pringles, cake, cupcakes, rice krispies, cheetos]
+snack_list = [pringles, cake, cupcake, rice_krispies, cheetos]
 
-@app.route('/snacks')
+@app.route('/snacks', methods=["GET", "POST"])
 def index():
-    return render_template('index.html', snacks=snacks)
+	if request.method == "POST":
+		snack_list.append(Snack(request.form.get('name'),request.form.get('kind')))
+		return redirect(url_for('index'))
+	return render_template('index.html', snack_list=snack_list)
+
+@app.route('/snacks/new')
+def new():
+	return render_template('new.html')
+
+@app.route('/snacks/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def show(id):
+	found_snack = [snack for snack in  snack_list if snack.id == id][0]
+	#if updating the snack
+	if request.method == b"PATCH":
+		found_snack.name = request.form.get('name')
+		found_snack.kind = request.form.get('kind')
+		return redirect(url_for('index'))
+
+	#if DELETING the snack
+	if request.method == b"DELETE":
+		snack_list.remove(found_snack)
+		return redirect(url_for('index'))
+	#if just showing info about the snack
+	return render_template('show.html', snack=found_snack)
+
+@app.route('/snacks/<int:id>/edit')
+def edit(id):
+	found_snack = [snack for snack in  snack_list if snack.id == id][0]
+	return render_template('edit.html', snack=found_snack)
+
 
 if __name__ == '__main__':
-    app.run(debug=True,port=3000)      
+	app.run(debug=True,port=3000)      
--- Unit-01/04-flask-crud/snack.py
+++ Unit-01/04-flask-crud/snack.py
@@ -2,7 +2,11 @@ class Snack():
 
     count = 1
 
-    def __init__(self,name):
+    def __init__(self,name, kind):
         self.name = name
+        self.kind = kind
         self.id = Snack.count
         Snack.count += 1
+
+    def __repr__(self):
+        return f"Snack #{self.id} -- Name: {self.name}; Kind: {self.kind}"
