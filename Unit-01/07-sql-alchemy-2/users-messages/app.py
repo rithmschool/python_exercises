8508b56fcd0375901de6d7313294a71920d5feda
michellecarey <genovese28@yahoo.com>
2017-11-30 20:48:03 -0800

    one too many

--- Unit-01/07-sql-alchemy-2/users-messages/app.py
+++ Unit-01/07-sql-alchemy-2/users-messages/app.py
@@ -15,10 +15,23 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	first_name = db.Column(db.Text)
 	last_name = db.Column(db.Text)
+	messages = db.relationship('Message', backref="user", lazy="dynamic")
 
 	def __init__(self, first_name, last_name):
 		self.first_name = first_name
-		self.last_name = last_name
+		self.last_name =  last_name
+
+class Message(db.Model):
+
+	__tablename__ = "messages"
+
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.Text)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+	def __init__(self, content, user_id):
+		self.content = content
+		self.user_id = user_id
 
 @app.route('/')
 def root():
@@ -30,19 +43,19 @@ def index():
   	new_user = User(request.form['first_name'], request.form['last_name'])
   	db.session.add(new_user)
   	db.session.commit()
-  	return redirect(url_for('index.html'))
+  	return redirect(url_for('index'))
   return render_template('users/index.html', users=User.query.all())
 
 @app.route('/users/new')
 def new():
 	return render_template('users/new.html')
 
-@app.route('/users/<int: id>/edit')
+@app.route('/users/<int:id>/edit')
 def edit(id):
 	found_user = User.query.get(id)
 	return render_template('users/edit.html', user=found_user)
 
-@app.route('/users/<int: id>', methods=["GET", "PATCH", "DELETE"])
+@app.route('/users/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def show(id):
 	found_user = User.query.get(id)
 	if request.method == b"PATCH":
@@ -50,12 +63,47 @@ def show(id):
 		found_user.last_name = request.form['last_name']
 		db.session.add(found_user)
 		db.session.commit()
-		return redirect(url_for('index.html'))
+		return redirect(url_for('index'))
 	if request.method == b"DELETE":
 		db.session.delete(found_user)
 		db.session.commit()
-		return redirect(url_for('index.html'))
-	return render_template('show/edit.html', user=found_user)
+		return redirect(url_for('index'))
+	return render_template('users/show.html', user=found_user)
+
+@app.route('/users/<int:user_id>/messages', methods=["GET", "POST"])
+def messages_index(user_id):
+  if request.method == "POST":
+  	new_message = Message(request.form['content'], user_id)
+  	db.session.add(new_message)
+  	db.session.commit()
+  	return redirect(url_for('messages_index', user_id=user_id))
+  return render_template('messages/index.html', user=User.query.get(user_id))
+
+@app.route('/users/<int:user_id>/messages/new', methods=["GET", "POST"])
+def messages_new(user_id):
+   return render_template('messages/new.html', user=User.query.get(user_id))
+
+@app.route('/users/<int:user_id>/messages/<int:id>/edit', methods=["GET", "POST"])
+def messages_edit(user_id, id):
+	found_message = Message.query.get(id)
+	return render_template('messages/edit.html', message=found_message)
+
+
+@app.route('/users/<int:user_id>/messages/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def messages_show(user_id, id):
+	found_message = Message.query.get(id)
+	if request.method == b"PATCH":
+		found_message.content = request.form['content']
+		db.session.add(found_message)
+		db.session.commit()
+		return redirect(url_for('messages_index', user_id=user_id))
+	if request.method == b"DELETE":
+		db.session.delete(found_message)
+		db.session.commit()
+		return redirect(url_for('messages_index', user_id=user_id))
+	return render_template('messages/show.html', message=found_message)
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True, port=3000)
