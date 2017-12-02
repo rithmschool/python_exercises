0a175230211ee751f49184e305308fd1a059cd4e
rebel_skum <jette.hernan@gmail.com>
2017-12-01 17:36:47 -0800

    put the D in CRUD

--- Unit-01/04-flask-crud/app.py
+++ Unit-01/04-flask-crud/app.py
@@ -11,7 +11,15 @@ cheetos = Snack(name = 'cheetos', kind = 'chips')
 skittles = Snack(name = 'skittles', kind = 'candy')
 peanuts = Snack(name = 'peanuts', kind = 'nuts')
 
-snack_list = [snickers, cheetos, skittles, peanuts]
+snack_list = [Snack(snickers, cheetos, skittles, peanuts)]
+
+def find_snack(id):
+    return [snack for snack in snack_list if snack.id == id][0]
+
+
+@app.route('/')
+def root():
+    return redirect(url_for('index'))
 
 @app.route('/snacks', methods=["GET", "POST"])
 def index():
@@ -24,12 +32,16 @@ def index():
 def new():
     return render_template('new.html')
 
-@app.route('/snacks/<int:id>')
+@app.route('/snacks/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def show(id):
-    found_snack = next(snack for snack in snack_list if snack.id == id)
+    found_snack = next(find_snack(id))
 
     if request.method == b"PATCH":
         found_snack.name = request.form['name']
+        return redirect(url_for('index'))
+
+    if request.method == b"DELETE":
+        snack_list.remove(found_snack)
         return redirect(url_for('index'))
 
     return render_template('show.html', snack=found_snack)
@@ -37,8 +49,10 @@ def show(id):
 @app.route('/snacks/<int:id>/edit')
 def edit(id):
     #refactored using a list comprehension
-    found_snack = next(snack for snack in snack_list if snack.id == id)
+    found_snack = next(find_snack(id))
     return render_template('edit.html', snack=found_snack)
+
+
 
 
 if __name__ == ("__main__"):
