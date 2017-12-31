fbcf6f3f65f0e745b7df5e266b3c169168b66add
rebel_skum <jette.hernan@gmail.com>
2017-12-30 20:31:32 -0500

    updating blueprints to files

--- Unit-02/01-blueprints/project/messages/views.py
+++ Unit-02/01-blueprints/project/messages/views.py
@@ -1,7 +1,59 @@
-from flask import Blueprint
+from flask import redirect, render_template, request, url_for, flash, Blueprint
+from project.models import Message, User
+from project.messages.forms import MessageForm, DeleteForm
+from project import db
 
 messages_blueprint = Blueprint(
     'messages',
     __name__,
     template_folder='templates'
 )
+
+@messages_blueprint.route('/users/<int:user_id>/messages', methods=["GET", "POST"])
+def index(user_id):
+    delete_form = DeleteForm()
+    if request.method == "POST":
+        message_form = MessageForm(request.form)
+        if message_form.validate():
+            new_message = Message(request.form['content'], user_id)
+            db.session.add(new_message)
+            db.session.commit()
+            flash('Message Created!')
+            return redirect(url_for('messages.index', user_id=user_id, form=message_form))
+        else:
+            return render_template('messages/new.html', user=User.query.get(user_id), form=message_form)
+    return render_template('messages/index.html', user=User.query.get(user_id), delete_form=delete_form)
+
+@messages_blueprint.route('/users/<int:user_id>/messages/new')
+def new(user_id):
+    message_form = MessageForm(request.form)
+    return render_template('messages/new.html', user=User.query.get(user_id), form=message_form)
+
+@messages_blueprint.route('/users/<int:user_id>/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def show(user_id, id):
+    found_message = Message.query.get(id)
+
+    if request.method == b'PATCH':
+        message_form = MessageForm(request.form)
+        if message_form.validate():
+            found_message.content = request.form['content']
+            db.session.add(found_message)
+            db.session.commit()
+            flash('Message Updated!')
+            return redirect(url_for('users.index', user_id=user_id))
+        return render_template('messages/edit.html', message=found_message, form=message_form)
+
+    elif request.method == b'DELETE':
+        delete_form = DeleteForm(request.form)
+        if delete_form.validate():
+            db.session.delete(found_message)
+            db.session.commit()
+            flash('Message Deleted!')
+        return redirect(url_for('messages.index', user_id=user_id))
+    return render_template('messages/show.html', message=found_message, form=message_form)
+
+@messages_blueprint.route('/users/<int:user_id>/messages/<int:id>/edit')
+def edit(user_id, id):
+    found_message = Message.query.get(id)
+    message_form = MessageForm(obj=found_message)
+    return render_template('messages/edit.html', message=found_message, form=message_form)
--- Unit-02/01-blueprints/project/users/views.py
+++ Unit-02/01-blueprints/project/users/views.py
@@ -1,8 +1,66 @@
-from flask import Blueprint
-from project.users.models import User
+from flask import redirect, render_template, request, url_for, flash, Blueprint
+from project.models import User
+from project.users.forms import UserForm, DeleteForm
+from project import db
 
-users_blueprint = BLueprint(
+users_blueprint = Blueprint(
     'users',
-    __name__'
+    __name__,
     template_folder='templates'
 )
+
+@users_blueprint.route('/', methods=["GET", "POST"])
+def index():
+    delete_form = DeleteForm()
+    if request.method == "POST":
+        form = UserForm(request.form)
+        if form.validate():
+            new_user = User(request.form['first_name'], request.form['last_name'])  # name from type in new.html#
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User Created!')
+            return redirect(url_for('index'))
+        else:
+            return render_template('users/new.html', form=form)
+    return render_template('users/index.html', users=User.query.all(), delete_form=delete_form)
+
+
+
+@users_blueprint.route('/new')
+def new():
+    user_form = UserForm()
+    return render_template('users/new.html', form=user_form)
+
+
+
+@users_blueprint.route('/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def show(id):
+    found_user = User.query.get(id)
+
+    if request.method == b"PATCH":
+        form = UserForm(request.form)
+        if form.validate():
+            found_user.first_name = form.first_name.data
+            found_user.last_name = form.last_name.data
+            db.session.add(found_user)
+            db.session.commit()
+            flash('User Updated!')
+            return redirect(url_for('index'))
+        return render_template('users/edit.html', user=found_user, form=form)
+
+    if request.method == b"DELETE":
+        delete_form = DeleteForm(request.form)
+        if delete_form.validate():
+            db.session.delete(found_user)
+            db.session.commit()
+            flash('User Deleted!')
+            return redirect(url_for('index'))
+    return render_template('users/show.html', user=found_user)
+
+
+@users_blueprint.route('/<int:id>/edit')
+def edit(id):
+    #refactored using a list comprehension
+    found_user = User.query.get(id)
+    user_form = UserForm(obj=found_user) #do not use request.form, use obj= to prepopulate the form
+    return render_template('users/edit.html', user=found_user, form=user_form)
